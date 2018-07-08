f922ee94dc7da1af6678b5b46b6fcd1a78306ac6
yqtang <yqtang1222@gmail.com>
2018-07-08 16:43:09 +0800

    add logrus package

---
+++ yuki.pkg.org/tools/logger/logger.go
@@ -0,0 +1,20 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+
+func setLogInfo(msgField string, infoText string) {
+    logrus.WithFields(logrus.Fields{
+		"msg": msgField,
+	}).Info(infoText)
+}
+
+func init() {
+	setLogInfo("Initial phase of logger", "in tools/logger/logger.go")
+}
+
+func InfoLog(msg string, info string) {
+	setLogInfo(msg, info)
+}
